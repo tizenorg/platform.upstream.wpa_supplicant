4c5ec9b963562f6fa047be6be645fec979dff401
Yu Jiung <jiung.yu@samsung.com>
2015-07-10 14:55:54 +0900

    [wpa_supplicant v2.4]Packaging: Initial packaging for Tizen 3.0
    Change-Id: I4a81b828476c41a946694d913474076f3e7fb0bd

---
+++ files/wpa_supp.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+start()
+{
+	HARDWARE_MODEL=`/bin/grep Hardware /proc/cpuinfo | /bin/awk "{print \\$3}"`
+
+	case $HARDWARE_MODEL in
+		"U1SLP" | "U1HD") /bin/echo "This is U1SLP"
+			/usr/sbin/wpa_supplicant -u -t -B -d -Dwext -f/opt/usr/data/network/wpa_supplicant.log
+		;;
+		"SLP7_C210")	 /bin/echo "This is C210"
+			/usr/sbin/wpa_supplicant -u -t -B -d -Dwext -f/opt/usr/data/network/wpa_supplicant.log
+		;;
+		"SLP10_C210")	/bin/echo "This is C210"
+			/usr/sbin/wpa_supplicant -u -t -B -d -Dwext -f/opt/usr/data/network/wpa_supplicant.log
+		;;
+		*)
+			/usr/sbin/wpa_supplicant -u -t -B -ddd -K -f/opt/usr/data/network/wpa_supplicant.log
+		;;
+	esac
+}
+
+stop()
+{
+	/usr/bin/killall wpa_supplicant
+	/bin/usleep 150000
+}
+
+case $1 in
+"start")
+start
+;;
+"stop")
+stop
+;;
+*)
+/bin/echo wpa_supp.sh [start] [stop]
+exit 1
+;;
+esac
